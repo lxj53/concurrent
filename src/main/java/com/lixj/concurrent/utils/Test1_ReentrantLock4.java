fcd827e147405142fb548eb32659f9d8f83cc9ba
lixj25188 <lixj25188@hundsun.com>
2020-11-04 08:39:20 +0800

    公平锁案例_未实现

--- src/main/java/com/lixj/concurrent/utils/Test1_ReentrantLock4.java
+++ src/main/java/com/lixj/concurrent/utils/Test1_ReentrantLock4.java
@@ -2,7 +2,9 @@ package com.lixj.concurrent.utils;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -22,32 +24,44 @@ import java.util.concurrent.locks.ReentrantLock;
 
 @Slf4j
 public class Test1_ReentrantLock4 {
+
+    // 创建锁的时候，增加是否是公平锁的标志，默认是非公平锁
     private static Lock reentrantLock = new ReentrantLock();
 
     public static void main(String[] args) throws InterruptedException {
-        Thread[] threads = new Thread[10];
+        Thread[] threads = new Thread[5000];
+
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+
+        AtomicInteger atomicInteger = new AtomicInteger(0);
+
         for (int i = 0; i < threads.length; i++) {
             int finalI = i;
             threads[i] = new Thread(() -> {
                 try {
                     reentrantLock.lock();
                     log.info("第{}个线程开始执行", Thread.currentThread().getName());
-                    if (finalI == 0) {
-                        try {
-                            TimeUnit.SECONDS.sleep(10);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
+                    if (Integer.parseInt(Thread.currentThread().getName()) != atomicInteger.incrementAndGet()) {
+                        log.error("####################出现非公平现象:{}", Thread.currentThread().getName());
                     }
+//                        countDownLatch.await();
+                    if (finalI == 0) {
+                        TimeUnit.SECONDS.sleep(20);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 } finally {
                     reentrantLock.unlock();
                 }
             }, String.valueOf(i + 1));
         }
-
         for (int i = 0; i < threads.length; i++) {
             threads[i].start();
+            // 依次启动线程，确保线程是按照顺序启动的
             TimeUnit.MILLISECONDS.sleep(2);
         }
+        log.info("主线程睡眠一会，再叫醒第一个线程");
+//            TimeUnit.SECONDS.sleep(2);
+//            countDownLatch.countDown();
     }
 }
