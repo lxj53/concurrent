f6a4899f3c04c791051c475b1350c5f6b02c0376
lixj25188 <lixj25188@hundsun.com>
2021-01-09 18:23:57 +0800

    增加测试类的注释

--- src/test/java/com/lixj/spring/step02_abstract_beanfactory_and_do_bean_initilizing_in_it/facory/BeanFactoryTest.java
+++ src/test/java/com/lixj/spring/step02_abstract_beanfactory_and_do_bean_initilizing_in_it/facory/BeanFactoryTest.java
@@ -23,12 +23,16 @@ import static org.junit.jupiter.api.Assertions.*;
 class BeanFactoryTest {
     @Test
     void test() {
+        // 1.创建bean容器
         BeanFactory beanFactory = new AutowireCapableBeanFactory();
 
+        // 2.注入bean
         BeanDefination beanDefination = new BeanDefination();
         beanDefination.setBeanClassName("com.lixj.spring.step02_abstract_beanfactory_and_do_bean_initilizing_in_it.HelloWorldService");
+        // 注册beanDefination时，会根据全限类名初始化bean对象
         beanFactory.registerBean("helloWorld", beanDefination);
 
+        // 3.获取bean
         HelloWorldService helloWorld = (HelloWorldService) beanFactory.getBean("helloWorld");
         helloWorld.hello();
     }
